f44358996b714e0c55fa44f299d8c9c091390d84
Rakibul <rakibulofficial12@gmail.com>
2021-09-13 23:38:02 +0600

    ok

--- Script/script.js
+++ Script/script.js
@@ -1,1 +1,9 @@
-console.log("Rakibul Islam")
+const sec = document.querySelector(".sec");
+const togal = document.querySelector(".togal");
+
+
+togal.addEventListener("click", () =>{
+    sec.classList.toggle("dark")
+})
+
+
